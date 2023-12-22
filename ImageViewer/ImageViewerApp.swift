735355a6a723e8c846668aabe7daea0cac2d47de
jkwon-dev <kwonejk@gmail.com>
2023-12-22 18:08:38 +0900

    Initial Commit

---
+++ ImageViewer/ImageViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewerApp.swift
+//  ImageViewer
+//
+//  Created by kwon eunji on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
